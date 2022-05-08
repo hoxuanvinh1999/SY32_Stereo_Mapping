9326ec2b723b3ed89f5719dcc0396e5eb9f3d079
Hoang Ho <hoangkplog@hoangkplog.localdomain>
2022-05-08 16:39:37 +0200

    remove test for dp method

--- test_optimize_methods.py
+++ test_optimize_methods.py
@@ -18,12 +18,3 @@ def test_spe():
     couple = sub_pixel_estimation((1, 3), (2, 7), (3, 13))
     assert almost_equal(couple[0], -0.5)
     assert almost_equal(couple[1], 0.75)
-
-
-def test_dp():
-    """Testing dynamic programming"""
-    image = np.arange(1, 13).reshape((3, 4))
-    image_abs = np.array([[14, 18, 22, 0], [30, 34, 38, 0], [0, 0, 0, 0]], np.uint32)
-    image_square = np.array([[66, 98, 138, 0], [242, 306, 378, 0], [0, 0, 0, 0]], np.uint32)
-    assert np.array_equal(dynamic_programming(image, 2, np.abs), image_abs)
-    assert np.array_equal(dynamic_programming(image, 2, np.square), image_square)
